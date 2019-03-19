344f0b32d74c42fe958ab0e3b1450f2b481de7a6
hafizhfauzi123 <hafizhfauzi1999@gmail.com>
2019-03-19 12:06:07 +0700

    Class dan Object Project

---
+++ Mahasiswa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassdanObject
+{
+    class Mahasiswa
+    {
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public string Ipk { get; set; }
+
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama: {0}", Nama);
+            Console.WriteLine("Nim: {0}", Nim);
+            Console.WriteLine("Ipk: {0}", Ipk);
+            Console.WriteLine("Telah Melakukan registrasi");
+        }
+        public void Isikrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses kes\n", Nama);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassdanObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            saya.Nim = "2345";
+            saya.Nama = "fauzi";
+            saya.Ipk = "3.95f";
+
+            kamu.Nim = "2317";
+            kamu.Nama = "suriyadi";
+            kamu.Ipk = "1.52f";
+
+            saya.Registrasi();
+            saya.Isikrs();
+
+            kamu.Registrasi();
+            kamu.Isikrs();
+
+
+            Console.ReadKey();
+        }
+    }
+}
